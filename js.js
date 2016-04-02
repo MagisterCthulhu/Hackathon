ff86d48b94a42ba2143633669284a7aa15106b8e
Akridian <pavel.ul@mail.ru>
2016-04-03 02:13:14 +0400

    clickable

--- js.js
+++ js.js
@@ -3,3 +3,12 @@ function goOut () {
 	x[0].classList.add('goOut');
 
 }
+
+function selectItem (sender) {
+	var x = document.getElementsByClassName("selected");
+	if (sender != x[0])
+	{
+		x[0].classList.remove('selected');
+		sender.classList.add('selected');
+	}
+}
